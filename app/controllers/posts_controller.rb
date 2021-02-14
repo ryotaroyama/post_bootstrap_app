d40edd7e5be5730c1130dae0619d341afdb3d005
ryotaroyamashita <ryotaro@ryotaroMacBook-Air.local>
2021-02-14 18:41:57 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
   def edit
